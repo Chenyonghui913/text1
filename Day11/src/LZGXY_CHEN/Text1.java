2b9a0b0d7c93725d9856af183d9045ec04504add
小陈 <188177532@qq.com>
2025-08-30 10:11:49 +0800

    添加输出

--- Day11/src/LZGXY_CHEN/Text1.java
+++ Day11/src/LZGXY_CHEN/Text1.java
@@ -5,5 +5,7 @@ import java.util.ArrayList;
 public class Text1 {
     public static void main(String[] args){
         System.out.println("hello");
+        System.out.println("hello,world");
+
     }
 }
